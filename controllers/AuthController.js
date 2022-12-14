0bee60158a3a32fe75d3f32ccaa52586985015c6
Sassus Capelo <synedcap@gmail.com>
2022-12-14 09:29:45 +0100

    validation of register and login request

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,25 @@
-var db = require("../models");
 var bcrypt = require("bcrypt");
+const db = require("../models");
 const jwt = require('jsonwebtoken');
+const {registerSchema} = require('../validations/RegisterRequest');
+const {loginSchema} = require('../validations/LoginRequest');
 
 exports.login = async function (req, res) {
   try {
+
+    //validation of the requests
+    const result = await loginSchema.validateAsync(req.body,{
+      abortEarly: false
+     });
+
     //search user
-    const user = await db.User.findOne({ where: { email: req.body.email } });
+    const user = await db.User.findOne({ where: { email: result.email } });
 
 
     if (!user) {
       res.status(404).json("user not found");
     } else {
-      //verify that password are the same
+      //check that password are the same
       const password_valid = await bcrypt.compare(
         req.body.password,
         user.password
@@ -41,14 +49,27 @@ exports.login = async function (req, res) {
       }
     }
   } catch (err) {
+
+     //check if error comes from joi validation
+     if (err.isJoi === true) {
+      err.status = 422;
+      res.status(422).json(err.details);
+    }
     res.status(500).json(err);
   }
 };
 
 exports.register = async function (req, res) {
   try {
+
+    //validation of the requests
+
+     const result = await registerSchema.validateAsync(req.body,{
+      abortEarly: false
+     });
+     
     //verify if user exists
-    const user = await db.User.findOne({ where: { email: req.body.email } });
+    const user = await db.User.findOne({ where: { email: result.email } });
     const salt = await bcrypt.genSalt(10);
 
     if (!user) {
@@ -63,10 +84,14 @@ exports.register = async function (req, res) {
         updatedAt: new Date(),
       });
 
-      console.log(newuser);
       res.status(200).json(newuser);
     }
   } catch (err) {
+    //check if error comes from joi validation
+    if (err.isJoi === true) {
+      err.status = 422;
+      res.status(422).json(err.details);
+    }
     res.status(500).json(err);
   }
 };
